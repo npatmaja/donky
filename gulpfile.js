2d93e8ee967b120a802a9726d1cbe2f62cee401a
npatmaja <noval.78@gmail.com>
2015-09-03 11:21:45 +0700

    Setup for travis_ci and coverall

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var exit = require('gulp-exit');
-
-gulp.task('test', [], function() {
-  return gulp.src('test/**/*.js', { read: false })
-    .pipe(mocha())
-    .pipe(exit());
-});
